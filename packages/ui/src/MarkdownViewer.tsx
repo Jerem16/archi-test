72ce7be945e62bee4c9e29f65eccf55cb8f9db0d
Jerem16 <jeremy.lemaignent@gmail.com>
2025-09-05 14:06:12 +0200

    v

--- packages/ui/src/MarkdownViewer.tsx
+++ packages/ui/src/MarkdownViewer.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
-export function MarkdownViewer({ markdown }: { markdown: string }) {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>;
+export function MarkdownViewer({ markdown }) {
+    return <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>;
 }
